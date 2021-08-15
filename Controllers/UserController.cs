d250c8d880f4b8b368287935e9ab6af75817607a
Monisha Jain <jain.monisha.17@gmail.com>
2021-08-15 21:15:57 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using OnlineMovieBooking.Models;
+
+namespace OnlineMovieBooking.Controllers
+{
+    // CS1038.cs 
+    public class UserController : Controller
+    {
+        private readonly string message;
+
+        // Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        //Registration Post Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration([Bind(Exclude = "IsEmailVerified, ActivationCode")] User user)
+        {
+            bool Status = false;
+            string message = "";
+            //
+            //Model Validation
+            if (ModelState.IsValid)
+            {
+                //Email is already exist
+                var isExist = IsEmailExist(user.EmailId);
+                if (isExist)
+                {
+                    ModelState.AddModelError("Email exist", "Email already exist");
+                    return View(user);
+                }
+
+               // #region Generate Activation Code
+                user.ActivationCode = Guid.NewGuid();
+
+               // #region Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword);
+                user.IsEmailVerified = false;
+               // #region Save Database
+
+                using (DatabaseMJEntities dc = new DatabaseMJEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+                    SendVerificationLinkEmail(user.EmailId, user.ActivationCode.ToString());
+                    message = "Registration is successfully done. Account activation link " + " has been sent to your email id:" + user.EmailId;
+                    Status = true;
+                }
+            }
+            else
+            {
+                message = "Ivalid Request";
+            }
+
+            ViewBag.Message = message;
+            ViewBag.Status = Status;
+            return View(user);
+        }
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (DatabaseMJEntities dc = new DatabaseMJEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailId == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string ActivationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + ActivationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+            var fromEmail = new MailAddress("dotnetawesome@gmail.com", "Dotnet Awesome");
+            var toEmail = new MailAddress(emailID);
+            var fromEmailPassword = "**********"; // Replace with actual password
+            string subject = "Your account is successfully created";
+
+            string body = "<br/><br/> We are excited to tell you that your Dotnet account is" +
+                "successfully created <br/> <br/> <a href='" + link + "'>" + link + "</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smpt.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+                smtp.Send(message);
+        }
+    }
+   // #endregion
+}
